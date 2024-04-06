9a8afc27c487358cf5170cbeaede1decb5c493d0
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-06 14:34:31 -0300

    att

--- src/components/SectionsPhoto/index.js
+++ src/components/SectionsPhoto/index.js
@@ -4,18 +4,18 @@ import Photo2 from '../../assets/admin.svg'
 import Photo3 from '../../assets/admin.svg'
 import Photo4 from '../../assets/admin.svg'
 
-import { Container, ContainerText } from "./styles";
+import { Container, ContainerImage } from "./styles";
 
 export function SectionsPhoto() {
 
   return (
     <Container>
-      <ContainerText>
+      <ContainerImage>
       <img src={Photo1} alt="img-geleria" />
       <img src={Photo2} alt="img-geleria" />
       <img src={Photo3} alt="img-geleria" />
       <img src={Photo4} alt="img-geleria" />
-      </ContainerText>
+      </ContainerImage>
     </Container>
   );
 }
--- src/components/SectionsPhoto/styles.js
+++ src/components/SectionsPhoto/styles.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
- 
+ width: 100vw;
 `;
 
-export const ContainerText = styled.div`
- 
+export const ContainerImage = styled.div`
+ width: 50px;
 
   @media screen and (max-width: 768px) {
  
