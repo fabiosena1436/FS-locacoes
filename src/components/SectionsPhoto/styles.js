7b2c45bd6f8c7fab489ed79fcbe020280ad5b2b5
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-06 16:20:45 -0300

    ajustando pagina

--- src/components/SectionsPhoto/styles.js
+++ src/components/SectionsPhoto/styles.js
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
- width: 100vw;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const ContainerImage = styled.div`
-display: flex;
-width: 80vh;
- img{
-  width: 50px;
- }
+  display: flex;
+  width: 80vh;
+  justify-content: center;
+  gap: 46px;
+  img {
+    width: 15vw;
+    padding: 81px 0;
+    
+  }
 
   @media screen and (max-width: 768px) {
- 
   }
 `;
