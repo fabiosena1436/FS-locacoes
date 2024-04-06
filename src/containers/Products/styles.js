924ad3ac878ef9a41500c856848f8983b79aa01c
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-06 11:08:27 -0300

    Alterando botao carrossel na responsividade

--- src/containers/Products/styles.js
+++ src/containers/Products/styles.js
@@ -1,30 +1,32 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
- background:#F2F2F2;
- min-height: calc(100vh - 72px);
+  background: #f2f2f2;
+  min-height: calc(100vh - 72px);
 
- .liNuNA{
-  display: none;
- }
+  .liNuNA {
+    display: none;
+  }
 
- .blnmcK{
-  display: none;
- }
+  .blnmcK {
+    display: none;
+  }
 
- .gCgsXv {
+  .gCgsXv {
     background-color: rgb(9 9 9 / 50%);
     box-shadow: 0 0 1px 3px rgb(3 3 3);
     display: none;
-}
+  }
+  @media screen and (max-width: 768px) {
+    .cQZIKn {
+      display: none;
+    }
+  }
 `;
 
 export const ProductsImg = styled.img`
   width: 100%;
 `;
-
-
-
 
 export const CategoryButton = styled.button`
   cursor: pointer;
@@ -35,15 +37,10 @@ export const CategoryButton = styled.button`
   font-size: 17px;
   line-height: 20px;
   padding-bottom: 5px;
-  margin-top:  40px  ;
+  margin-top: 40px;
   @media screen and (max-width: 768px) {
-    
     font-size: 0.8rem;
-   
   }
-
-
- 
 `;
 export const CategoriesMenu = styled.div`
   display: flex;
@@ -63,6 +60,5 @@ export const ProductsContainer = styled.div`
   @media screen and (max-width: 768px) {
     grid-template-columns: repeat(1, 0fr);
     gap: 10px;
-   
   }
 `;
